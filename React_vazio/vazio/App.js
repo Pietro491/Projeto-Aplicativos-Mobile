99abeb8e1c9d759fdf607ee50827993a005baf27
Pietro491 <pietromoraes491@gmail.com>
2026-06-01 12:31:02 +0000

    versao2

--- React_vazio/vazio/App.js
+++ React_vazio/vazio/App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-web';
 
 export default function App() {
   return (
+    <scroolView>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Teste de tela</Text>
     </View>
+    <View style={styles.}> 
+      <Text>
+     
+
+      
+      <Text/>
+      <View/>
+      <TextInput style=({height:40, borderColor: "orange", borderWidth: 2, width:  200,})>
+      </Text>
+
+    <scroolView/>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    margintop: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  cont_texto {
+    height: 1250,
+    backgroundColor: "yellow",
+
+  }
 });
